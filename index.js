950857bd39cfea1e4727e8e2ca9ff6b66a47d9e2
Mohammad Nazmul <141314437+nazmull139@users.noreply.github.com>
2025-01-03 16:57:16 +0600

    Update index.js
    cors change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://react-snake-game-frontend.vercel.app',
   credentials: true
 }))
 
